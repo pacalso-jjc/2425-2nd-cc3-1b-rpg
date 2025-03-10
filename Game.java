bd6d8b45387ae63c2ec8f5acaa37953d2fc04e0d
pacalso-jjc <jjcpacalso@uc-bcf.edu.ph>
2025-03-10 09:05:19 +0800

    Add start method to Game class

--- Game.java
+++ Game.java
@@ -1,6 +1,11 @@
 public class Game{
+    private boolean isRunning;
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+    }
+
+    public void start(){
+        System.out.println("Game is running");
     }
 }
