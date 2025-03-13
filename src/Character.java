5faf79eaed2714b5aa02513472df47d93a6f220c
sumalinogcks <css9223@students.uc-bcf.edu.ph>
2025-03-13 16:21:17 +0800

    doc-76: Created a new java file(Character.java)

---
+++ src/Character.java
@@ -0,0 +1,27 @@
+public class Character{
+	private String sex;
+	private String race;
+	private int exp;
+	private int charisma;
+	
+	public void addToInventory(Item item){
+		
+	}
+	
+	public void sellItems(Item item){
+		
+	}
+	
+	public void equipItems(Item item){
+		
+	}
+	
+	public void move(Direction direction){
+		
+	}
+
+	public void regenerate(amountToRegenerate regenete){
+		
+	}
+	
+}
