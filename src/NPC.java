5445221a750e9da13d5b18e2174556701bf66de4
rominesgmd <gdr3421@students.uc-bcf.edu.ph>
2025-03-15 12:29:06 +0800

    Created and Added the code for NPC.java

---
+++ src/NPC.java
@@ -0,0 +1,10 @@
+public class NPC extends Character{
+    private List<Quest> questList;
+    Public NPC(String name, int hp, int level){
+        this.name=name;
+        this.hp=hp;
+        this.level=level;
+
+    }
+
+}
