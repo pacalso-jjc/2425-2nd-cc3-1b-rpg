44610e71e95414fe80bc3223f016594f1fe370bf
Marinell Aquino <mva6253@students.uc-bcf.edu.ph>
2025-03-17 09:59:12 +0800

    Update InputHandler.java
    Removed close method

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -5,16 +5,11 @@ public class InputHandler {
 
     // Constructor initializes the Scanner
     public InputHandler() {
-        sc = new Scanner(System.in);
+        this.sc = new Scanner(System.in);
     }
 
     // Method to get user input as a String
     public String getInput() {
         return sc.nextLine();  // Reads and returns the user's input
-    }
-
-    // Close the scanner when it's no longer needed
-    public void close() {
-        sc.close();
     }
 }
