9fd40ca17740f3344f553abe4c9afeec854a0c0d
Jaron Clyde Puyao <jsp4995@students.uc-bcf.edu.ph>
2025-03-17 20:15:09 +0800

    Updated enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,22 +1,8 @@
 public class Enemy extends Character {
-    private int level;
     private String type;
 
     public Enemy(String name, int level, String type) {
-        this.setName(name);
-        this.level = level;
+        super(name, level); 
         this.type = type;
-    }
-
-    public void attack(Entity target) {
-        
-    }
-
-    public void takeDamage(int damage) {
-        
-    }
-
-    public void dropLoot() {
-        
     }
 }
