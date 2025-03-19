b99b227ef13439a114d70e10f73de1d21d2fe6da
ambrocioreg <rga5803@students.uc-bcf.edu.ph>
2025-03-19 20:00:53 +0800

    Update Merchant.java

--- src/Merchant.java
+++ src/Merchant.java
@@ -3,8 +3,12 @@ import java.util.List;
 public class Merchant extends NPC {
   List<String> stock = new ArrayList<>();
   
-  public boolean buyItem (Character buyer, Item item, int quantity) {}
-  public boolean sellItem (Character seller, Item item, int quantity) {}
+  public boolean buyItem (Character buyer, Item item, int quantity) {
+    return true;
+}
+  public boolean sellItem (Character seller, Item item, int quantity) {
+    return true,
+}
   protected void setPrices (Item item, float newPrice) {}
   protected void restockItems() {}
 }
