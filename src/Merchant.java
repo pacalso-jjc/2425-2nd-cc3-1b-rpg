5b7cade14e719b456bb4ee9da002a75b283bd00f
ambrocioreg <rga5803@students.uc-bcf.edu.ph>
2025-03-19 08:18:12 +0800

    Update Merchant.java based its Class Diagram.
    Class Diagram is still in review.

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,3 +1,10 @@
 import java.util.List;
 
-public Class
+public class Merchant extends NPC {
+  List<String> stock = new ArrayList<>();
+  
+  public boolean buyItem (Character buyer, Item item, int quantity) {}
+  public boolean sellItem (Character seller, Item item, int quantity) {}
+  protected void setPrices (Item item, float newPrice) {}
+  protected void restockItems() {}
+}
