0f57117c54102da3f9e3d810c609419d907243c4
sumalinogcks <css9223@students.uc-bcf.edu.ph>
2025-04-02 12:53:58 +0800

    doc-75: updated the code based on the current class diagram in wiki

--- src/Entity.java
+++ src/Entity.java
@@ -1,12 +1,15 @@
 import java.util.List;
 
 public abstract class Entity {
-    private String name;
-    private List<Skill> skills;
     private int exp;
-    private int hp;
-    private int mana;
-    private int stamina;
+    private int maxHp; 
+    private int maxMana;
+    private int maxStamina;
+    private int currentHp;
+    private int currentMana;
+    private int currentStamina;
+    private Statistic stas;
+    private List<Skill> skills;
     private Inventory inventory;
     private List<Item> equipments;
 
